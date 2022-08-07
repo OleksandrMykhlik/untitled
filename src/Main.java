910c9de93a7e15f42984713f15fccf67c8bbcd24
Oleksandr M <imdominofoxy@gmail.com>
2022-08-07 15:59:47 +0300

    vanya saw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+    static String verdict;
+    static int inp;
+
+    static void answer (String result) {
+        System.out.println("Похоже, "+ inp + " - " + result +  " число.");
     }
+
+    static boolean calc (int vol) {
+        return vol%2 == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Введите ваше число:");
+        Scanner myInput = new Scanner(System.in);
+        inp = myInput.nextInt();
+
+        if (inp==0){
+            System.out.println("Ох, нет! Да у вас же... НОЛЬ!");
+
+        } else {
+            if (calc(inp)) {
+                verdict = ("четное");
+            } else {
+                verdict = ("нечетное");
+            }
+
+            answer(verdict);
+        }
+    }
+
 }
